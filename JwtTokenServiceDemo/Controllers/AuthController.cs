ebda5cf700c038eef33e1cd6ebde0b6c50c29178
Mike Costarella <mcosti@sprynet.com>
2023-06-08 10:02:26 -0400

    Add project files.

---
+++ JwtTokenServiceDemo/Controllers/AuthController.cs
@@ -0,0 +1,62 @@
+﻿using JwtTokenServiceDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace JwtTokenServiceDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public static User user = new User();
+        private readonly IConfiguration configuration;
+
+        public AuthController(IConfiguration configuration) {
+            this.configuration = configuration;
+        }
+
+        [HttpPost("register")]
+        public ActionResult<User> Register(UserDto request)
+        {
+            var passwordHash = BCrypt.Net.BCrypt.HashPassword(request.Password);
+            user.Username = request.Username;
+            user.PasswordHash = passwordHash;
+
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public ActionResult<User> Login(UserDto request)
+        {
+            // Look for user in db
+            if (user.Username != request.Username)
+            {
+                return BadRequest("User not found");
+            }
+            if (!BCrypt.Net.BCrypt.Verify(request.Password, user.PasswordHash))
+            {
+                return BadRequest("Wrong password");
+            }
+            var token = CreateToken(user);
+            return Ok(token);
+        }
+
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Username)
+            };
+            var secretToken = configuration.GetSection("AppSettings:Token");
+            var encoding = Encoding.UTF8.GetBytes(secretToken.Value!);
+            var key = new SymmetricSecurityKey(encoding);
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            var jwtToken = new JwtSecurityToken(claims: claims, expires: DateTime.Now.AddDays(1), signingCredentials: credentials);
+            var jwt = new JwtSecurityTokenHandler().WriteToken(jwtToken);
+            return jwt;
+        }
+    }
+}
---
+++ JwtTokenServiceDemo/Models/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace JwtTokenServiceDemo.Models
+{
+    public class UserDto
+    {
+        public required string Username { get; set; }
+
+        public required string Password { get; set; }
+    }
+}
